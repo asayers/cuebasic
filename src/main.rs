5dc086b5bc30b3d88ea2c77adf3a18f141e1b196
Alex Sayers <alex@asayers.com>
2024-03-27 22:16:39 +0900

    Read from stdin if no file is given

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use bpaf::{Bpaf, Parser};
 use cuebasic::*;
 use logos::Logos;
-use std::path::PathBuf;
+use std::{io::Read, path::PathBuf};
 
 #[derive(Bpaf)]
 struct Opts {
@@ -13,19 +13,26 @@ struct Opts {
     last_write_wins: bool,
     /// The source file.  Reads from stdin if not specified
     #[bpaf(positional("PATH"))]
-    file: PathBuf,
+    file: Option<PathBuf>,
 }
 
 fn main() -> anyhow::Result<()> {
     tracing_subscriber::fmt::init();
     let opts = opts().to_options().run();
-    let file = std::fs::read_to_string(opts.file)?;
+    let source = match opts.file {
+        Some(path) => std::fs::read_to_string(path)?,
+        None => {
+            let mut buf = String::new();
+            std::io::stdin().read_to_string(&mut buf)?;
+            buf
+        }
+    };
     if opts.tokens {
-        for token in Token::lexer(&file) {
+        for token in Token::lexer(&source) {
             eprintln!("{token:?}");
         }
     }
-    let tree = parse(Token::lexer(&file))?;
+    let tree = parse(Token::lexer(&source))?;
     if opts.unmerged {
         for (path, target) in &tree {
             eprintln!("{path}: {target}");
